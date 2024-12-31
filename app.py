0ef77d5c211d3b94d4568d38161078f488d6a18c
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-12-31 08:44:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ st.header("🎉 ¡Haz tu deseo para el 2025! 🎉")
 # Integrar formulario de Google Forms
 st.markdown(
     """
-    <iframe src="https://docs.google.com/forms/d/1sJV7KiPi3pOPeF-r-48BB9yGAol82YYZjpbnOc0EbLk/edit" 
+    <iframe src="https://docs.google.com/forms/d/1AcqZQhhPe3poepvkv5OjoXirFBFFarHeOUrAKLf5uc0/edit" 
             width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
     """,
     unsafe_allow_html=True
